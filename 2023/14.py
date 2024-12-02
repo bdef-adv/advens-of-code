cadc6c2dfb36e0b48bb0d548d0e1f7fb62ca727f
bdef-adv <baptiste.defrance@advens.fr>
2024-12-02 15:36:17 +0100

    280 days to get the part 2 answer -_-

--- 2023/14.py
+++ 2023/14.py
@@ -10,38 +10,49 @@ FILENAME_PART2_EXT = ""
 
 def display_map(puzzle):
     for line in puzzle:
-        print(line)
+        print(''.join(line))
+
+def get_line(line):
+    new_line = line
+    moved = True
+    while moved:
+        moved = False
+        count_gap = 0
+        curr_line = []
+        for x, ch in enumerate(new_line):
+            if ch == '.':
+                count_gap += 1
+            elif ch == 'O' and count_gap:
+                curr_line += ['O'] + ['.']*count_gap
+                count_gap = 0
+                curr_line += new_line[x+1::]
+                moved = True
+                break
+            elif count_gap:
+                curr_line += ['.'] * (count_gap) + [ch]
+                count_gap = 0
+            else:
+                curr_line += [ch]
+        if count_gap:
+            curr_line += ['.'] * count_gap
+        new_line = curr_line
+    return new_line
 
 def get_slipped_map_north(puzzle):
     flipped = list(zip(*puzzle))
     new_map = []
     len_line = len(flipped[0])
     for line in flipped:
-        new_line = line
-        moved = True
-        while moved:
-            moved = False
-            count_gap = 0
-            curr_line = ""
-            for x, ch in enumerate(new_line):
-                if ch == '.':
-                    count_gap += 1
-                elif ch == 'O' and count_gap:
-                    curr_line += 'O' + '.'*count_gap
-                    count_gap = 0
-                    curr_line += ''.join(new_line[x+1::])
-                    moved = True
-                    break
-                elif count_gap:
-                    curr_line += '.' * (count_gap) + ch
-                    count_gap = 0
-                else:
-                    curr_line += ch
-            if count_gap:
-                curr_line += '.' * count_gap
-            new_line = curr_line
-        new_map.append(new_line)
-    return new_map
+        new_map.append(get_line(line))
+    return list(zip(*new_map))
+
+def get_slipped_map_south(puzzle):
+    flipped = list(zip(*puzzle[::-1]))
+    new_map = []
+    len_line = len(flipped[0])
+    for line in flipped:
+        new_map.append(get_line(line))
+    return list(zip(*new_map))[::-1]
 
 def solution_part1(filename):
     """ PART 1
@@ -53,7 +64,6 @@ def solution_part1(filename):
             puzzle.append(line)
 
         puzzle = get_slipped_map_north(puzzle)
-        puzzle = [''.join(y) for y in list(zip(*puzzle))]
         count = 0
         len_puzzle = len(puzzle)
 
@@ -72,10 +82,31 @@ def solution_part2(filename):
             line = _line.rstrip()
             puzzle.append(line)
 
-        for i in progressbar(range(4)):
-            puzzle = get_slipped_map_north(puzzle)
-            display_map([''.join(y) for y in list(zip(*puzzle))])
-            print("---")
+        for i in progressbar(range(1000000000)):
+            puzzle = get_slipped_map_north(puzzle) # Flipped north
+            #print("North")
+            #display_map(puzzle)
+
+            #print("West")
+            new_puzzle = []
+            for line in puzzle:
+                new_puzzle.append(get_line(line))
+            puzzle = new_puzzle
+
+            #display_map(puzzle)
+
+            #print("South")
+            puzzle = get_slipped_map_south(puzzle)
+            #display_map(puzzle)
+
+            #print("East")
+            puzzle = [y[::-1] for y in puzzle]
+            new_puzzle = []
+            for line in puzzle:
+                new_puzzle.append(get_line(line))
+            puzzle = [y[::-1] for y in new_puzzle]
+            #display_map(puzzle)
+
 
         count = 0
         len_puzzle = len(puzzle)
@@ -95,7 +126,7 @@ if __name__ == "__main__":
 
     print("--- Part Two ---")
     print("Test result:")
-    print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.test.txt"))
+    #print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.test.txt"))
 
     print("Result:")
-    #print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.txt"))
+    print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.txt"))
