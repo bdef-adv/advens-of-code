201da8c3f3f5e682d3ed7cd60d958d15615100f4
bdef-adv <baptiste.defrance@advens.fr>
2024-12-02 15:36:16 +0100

    04 part 2 ok

--- 2023/04.py
+++ 2023/04.py
@@ -28,28 +28,36 @@ def solution_part1(filename):
 
         return final_score
 
+def get_winning_cards(cards, original_cards, rec=0):
+    """ PART 2 RECURSIVE SOL
+    """
+    won_cards = []
+
+    for card, left, right in cards:
+        _, x = card.split()
+        x = int(x) - 1
+        numbers = [num for num in right if num in left]
+        won_cards.append((card, numbers))
+        if numbers:
+            won_cards += get_winning_cards(original_cards[x+1:x + 1 + len(numbers):], original_cards, rec=rec+1)
+    return won_cards
+
 
 def solution_part2(filename):
     """ PART 2
     """
     with open(filename, "r", encoding="utf-8") as file:
         final_score = 0
+        cards = []
         for _line in file:
             line = _line.rstrip()
             card, data = line.split(": ")
             left, right = map(str.split, data.split(' | '))
-            points = 0
-            numbers = []
-            for number in right:
-                if number in left:
-                    if not points:
-                        points = 1
-                    else:
-                        points *= 2
-                    numbers.append(number)
-            final_score += points
+            cards.append((card, left, right))
 
-        return final_score
+        won_cards = get_winning_cards(cards, cards)
+
+        return len(won_cards)
 
 
 if __name__ == "__main__":
