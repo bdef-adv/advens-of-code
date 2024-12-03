c76b31b2ff7b4d112e8929a2d0638f94f863951a
bdef-adv <baptiste.defrance@advens.fr>
2024-12-03 09:58:09 +0100

    day 3

--- rust/src/years/y2024/day03.rs
+++ rust/src/years/y2024/day03.rs
@@ -1,19 +1,49 @@
-fn part01(_file_contents: &str) -> u32 {
+use regex::Regex;
+
+fn part01(file_contents: &str) -> u32 {
     /*
         Part 1
      */
+    let re_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let mut sum: u32 = 0;
-    sum += 0;
+
+    for (_, [left, right]) in re_mul.captures_iter(file_contents).map(|c| c.extract()) {
+        sum += left.parse::<u32>().unwrap() * right.parse::<u32>().unwrap();
+    }
+
     return sum;
 }
 
 
-fn part02(_file_contents: &str) -> u32 {
+fn part02(file_contents: &str) -> u32 {
     /*
         Part 2
      */
     let mut sum: u32 = 0;
-    sum += 0;
+    let re_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut disabled_buffer = String::from("");
+    let mut buffer = String::from("");
+    let mut enabled = true;
+    for char in file_contents.chars() {
+        if enabled {
+            buffer.push(char);
+        } else {
+            disabled_buffer.push(char);
+        }
+        if buffer.ends_with("don't()") {
+            enabled = false;
+        }
+        if disabled_buffer.ends_with("do()") {
+            enabled = true;
+            disabled_buffer = String::from("");
+        }
+    }
+
+    for (_, [left, right]) in re_mul.captures_iter(buffer.as_str()).map(|c| c.extract()) {
+        sum += left.parse::<u32>().unwrap() * right.parse::<u32>().unwrap();
+    }
+
     return sum;
 }
 
