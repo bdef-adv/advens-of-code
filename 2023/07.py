4147a17d4b0db52aa94590388ecd64f9850042e1
bdef-adv <baptiste.defrance@advens.fr>
2024-12-08 14:30:30 +0100

    day 7 not working debug code

---
+++ 2023/07.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python
+
+from pathlib import Path
+import sys
+from functools import cmp_to_key
+
+FILENAME = sys.argv[0]
+FILENAME_TRUNC = Path(FILENAME).stem
+FILENAME_PART2_EXT = ""
+
+SCORING = "AKQJT98765432J" #['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+RANK = {"five of a kind": 6, "four of a kind": 5, "full house": 4, "three of a kind": 3, "two pairs": 2, "one pair": 1, "high card": 0}
+
+def compare_cards(hand1, hand2):
+    if hand1[1] < hand2[1]:
+        return -1
+    elif hand1[1] > hand2[1]:
+        return 1
+    
+    for x, ch in enumerate(hand1[0]):
+        lindex, rindex = SCORING.find(ch), SCORING.find(hand2[0][x])
+        if lindex > rindex:
+            return -1
+        elif lindex < rindex:
+            return 1
+        else:
+            continue
+
+    return 0
+
+def compare_counts(hand1, hand2):
+    lindex, rindex = hand1[1], hand2[1]
+    if lindex < rindex:
+        return 1
+    elif lindex > rindex:
+        return -1
+
+    lindex, rindex = SCORING.find(hand1[0]), SCORING.find(hand2[0])
+    if lindex < rindex:
+        return 1
+    elif lindex > rindex:
+        return -1
+    return 0
+
+
+def solution_part1(filename):
+    """ PART 1
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        hands = []
+        for _line in file:
+            line = _line.rstrip()
+            cards, bid = line.split()
+
+            # card: count
+            count_cards = {}
+            for card in cards:
+                count_cards[card] = 1 if not card in count_cards else count_cards[card] + 1
+
+            score = ""
+            pairs = []
+            threes = []
+            for card, count in count_cards.items():
+                if count == 5:
+                    score = "five of a kind"
+                    break
+                if count == 4:
+                    score = "four of a kind"
+                if count == 3:
+                    threes.append(card)
+                if count == 2:
+                    pairs.append(card)
+
+            if len(pairs) == 1 and len(threes) == 1:
+                score = "full house"
+            elif len(pairs) == 2:
+                score = "two pairs"
+            elif len(threes) == 1:
+                score = "three of a kind"
+            elif len(pairs) == 1:
+                score = "one pair"
+            elif not score:
+                score = "high card"
+
+            hands.append((cards, RANK[score], int(bid)))
+        score = 0
+        hands = sorted(hands, key=cmp_to_key(compare_cards))
+        for x, hand in enumerate(hands):
+            score += hand[2] * (x+1)
+
+        return score
+
+
+def solution_part2(filename):
+    """ PART 2
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        hands = []
+        for _line in file:
+            line = _line.rstrip()
+            cards, bid = line.split()
+
+            jokers = 0
+
+            # card: count
+            count_cards = {}
+            for card in cards:
+                if card == 'J':
+                    jokers += 1
+                else:
+                    count_cards[card] = 1 if not card in count_cards else count_cards[card] + 1
+
+            count_cards_list = sorted(count_cards.items(), key=cmp_to_key(compare_counts))
+
+            debug = False
+            if jokers:
+                debug = True
+                print(cards, count_cards_list, jokers)
+
+            len_counts = len(count_cards_list)
+            score = ""
+            pairs = []
+            threes = []
+            most_count = count_cards_list[0][1]
+            next_most = 0 if len_counts == 1 else count_cards_list[1][1]
+            if len_counts == 5 or (len_counts == 4 and count_cards_list[0][1] != 2 and not jokers):
+                score = "high card"
+            elif len_counts == 4 and not jokers:
+                score = "one pair"
+            elif len_count == 4 and jokers == 1:
+                score = "three of a kind"
+            elif len_count == 3 and not jokers:
+                score = ""
+            for card, count in count_cards_list:
+                if count == 5:
+                    score = "five of a kind"
+                    break
+                if count == 4:
+                    if jokers == 1:
+                        score = "five of a kind"
+                        jokers -= 1
+                    else:
+                        score = "four of a kind"
+                    break
+                if count == 3:
+                    if jokers == 2:
+                        score = "five of a kind"
+                        jokers -= 2
+                        break
+                    elif jokers == 1:
+                        score = "four of a kind"
+                        jokers -= 1
+                        break
+                    else:
+                        threes.append(card)
+                if count == 2:
+                    if jokers == 1:
+                        jokers -= 1
+                        threes.append(card)
+                    elif jokers == 2:
+                        score = "four of a kind"
+                        jokers -= 2
+                        break
+                    elif jokers == 3:
+                        jokers -= 3
+                        score = "five of a kind"
+                        break
+                    else:
+                        pairs.append(card)
+                if count == 1:
+                    if jokers == 1:
+                        jokers -= 1
+                        pairs.append(card)
+                    elif jokers == 2:
+                        jokers -= 2
+                        threes.append(card)
+                    elif jokers == 3:
+                        jokers -= 3
+                        score = "four of a kind"
+                    elif jokers == 4:
+                        jokers -= 4
+                        score = "five of a kind"
+
+            if len(pairs) == 1 and len(threes) == 1:
+                score = "full house"
+            elif len(pairs) == 2:
+                score = "two pairs"
+            elif len(threes) == 1:
+                score = "three of a kind"
+            elif len(pairs) == 1:
+                score = "one pair"
+            elif not score:
+                score = "high card"
+
+            if debug:
+                print(score)
+            hands.append((cards, RANK[score], int(bid)))
+        score = 0
+        hands = sorted(hands, key=cmp_to_key(compare_cards))
+        for x, hand in enumerate(hands):
+            score += hand[2] * (x+1)
+
+        print(hands)
+
+        return score
+
+
+if __name__ == "__main__":
+    print("--- Part One ---")
+    print("Test result:")
+    print(solution_part1(f"input.{FILENAME_TRUNC}.test.txt"))
+
+    print("Result:")
+    print(solution_part1(f"input.{FILENAME_TRUNC}.txt"))
+
+    print("--- Part Two ---")
+    print("Test result:")
+    print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.test.txt"))
+
+    print("Result:")
+    print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.txt"))
