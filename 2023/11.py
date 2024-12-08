fe6926cb65825d1e3d79dd5d723cafabbb6f2e34
bdef-adv <baptiste.defrance@advens.fr>
2024-12-08 14:30:30 +0100

    day 11 baby

--- 2023/11.py
+++ 2023/11.py
@@ -2,25 +2,95 @@
 
 from pathlib import Path
 import sys
+from itertools import combinations
 
 FILENAME = sys.argv[0]
 FILENAME_TRUNC = Path(FILENAME).stem
 FILENAME_PART2_EXT = ""
 
+
 def solution_part1(filename):
     """ PART 1
     """
+    offset = 1
     with open(filename, "r", encoding="utf-8") as file:
-        for _line in file:
+        universe = []
+        vertical_stars_indexes = []
+        stars = []
+        for y, _line in enumerate(file):
             line = _line.rstrip()
+            universe.append(line)
+            if not vertical_stars_indexes:
+                vertical_stars_indexes = [0 for ch in line]
+            for x, ch in enumerate(line):
+                if ch == '#':
+                    vertical_stars_indexes[x] += 1
 
+        # find stars
+        offset_y = 0
+        for y, line in enumerate(universe):
+            offset_x = 0
+            if '#' not in line:
+                offset_y += offset
+            for x, ch in enumerate(line):
+                if vertical_stars_indexes[x] == 0:
+                    offset_x += offset
+                    continue
+                if ch == '#':
+                    stars.append((y + offset_y, x + offset_x))
+
+        # calculate differences
+        differences = []
+        for iterat in combinations(stars, 2):
+            starleft, starright = iterat
+            y_left, x_left = starleft
+            y_right, x_right = starright
+
+            differences.append(abs(x_right - x_left) + abs(y_right - y_left))
+
+        return sum(differences)
 
 def solution_part2(filename):
     """ PART 2
     """
+    offset = 1000000
     with open(filename, "r", encoding="utf-8") as file:
-        for _line in file:
+        universe = []
+        vertical_stars_indexes = []
+        stars = []
+        for y, _line in enumerate(file):
             line = _line.rstrip()
+            universe.append(line)
+            if not vertical_stars_indexes:
+                vertical_stars_indexes = [0 for ch in line]
+            # find empty vertical universe spots by counting where the stars are
+            for x, ch in enumerate(line):
+                if ch == '#':
+                    vertical_stars_indexes[x] += 1
+
+        # find stars
+        offset_y = 0
+        for y, line in enumerate(universe):
+            offset_x = 0
+            if '#' not in line:
+                offset_y += offset - 1
+            for x, ch in enumerate(line):
+                if vertical_stars_indexes[x] == 0:
+                    offset_x += offset - 1
+                    continue
+                if ch == '#':
+                    stars.append((y + offset_y, x + offset_x))
+
+        # calculate differences
+        differences = []
+        for iterat in combinations(stars, 2):
+            starleft, starright = iterat
+            y_left, x_left = starleft
+            y_right, x_right = starright
+
+            differences.append(abs(x_right - x_left) + abs(y_right - y_left))
+
+        return sum(differences)
 
 
 if __name__ == "__main__":
