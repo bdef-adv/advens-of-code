9c1e55ab2c6f262d4b905df8c74a3459de00126d
bdef-adv <baptiste.defrance@advens.fr>
2024-12-08 14:30:31 +0100

    day 2 2024; cleaned up module imports; added start.sh script; prepared day 3 2024

--- rust/src/years/y2023/mod.rs
+++ rust/src/years/y2023/mod.rs
@@ -5,6 +5,8 @@ use crate::utils;
 pub fn run_day(day: &mut u8) -> (i32, i32) {
     let contents = utils::get_day_input_content(day, 2023);
 
+    println!("Running day {day} of year 2023");
+
     match day {
         1 => {
             let results = day01::get_day_results(&contents.to_string());
---
+++ rust/src/years/y2024/day02.rs
@@ -0,0 +1,91 @@
+fn report_safe(line_values: &Vec<i32>) -> bool {
+    let mut last_value: i32 = line_values[1];
+    let desc: bool = line_values[1] < line_values[0];
+
+    let mut diff: i32 = (line_values[0] - line_values[1]).abs();
+
+    if diff > 3 || diff == 0 {
+        return false;
+    }
+
+    for value in line_values[2..].iter() {
+        if *value < last_value && !desc {
+            return false;
+        } else if *value > last_value && desc {
+            return false;
+        }
+
+        diff = (*value - last_value).abs();
+
+        if diff > 3 || diff == 0 {
+            return false;
+        }
+
+        last_value = *value;
+    }
+
+    return true;
+}
+
+
+fn part01(file_contents: &str) -> u32 {
+    /*
+        Part 1
+     */
+    let mut sum: u32 = 0;
+
+    for line in file_contents.lines() {
+        let line_values: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        if report_safe(&line_values) {
+            sum += 1
+        }
+    }
+
+    return sum;
+}
+
+
+fn problem_dampener(line_values: &Vec<i32>) -> bool {
+    let new_len = line_values.len() - 1;
+    for index in 0..(new_len+1) {
+        let new_values: Vec<i32> = line_values.iter().enumerate()
+            .filter_map(|(i, &value)| {
+                if i != index {
+                    Some(value)
+                } else {
+                    None
+                }
+            })
+            .collect();
+        if report_safe(&new_values) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+fn part02(file_contents: &str) -> u32 {
+    /*
+        Part 2
+     */
+    let mut sum: u32 = 0;
+    
+    for line in file_contents.lines() {
+        let line_values: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        if problem_dampener(&line_values) {
+            sum += 1
+        }
+    }
+    
+    return sum;
+}
+
+
+pub fn get_day_results(file_contents: &str) -> (u32, u32) {
+    /*
+        Return this day's results as a tuple
+     */
+    (part01(&file_contents).try_into().unwrap(), part02(&file_contents).try_into().unwrap())
+}
