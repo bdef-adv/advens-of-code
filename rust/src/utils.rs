c95852d53c339423c6fefe7344859563a390f95e
bdef-adv <baptiste.defrance@advens.fr>
2024-12-08 14:30:31 +0100

    move inputs

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -2,10 +2,6 @@ use std::fs::File;
 use std::io::{self, Read};
 use chrono::prelude::*;
 
-pub fn unsigned_to_signed(src: &(u64, u64)) -> (i64, i64) {
-    return (src.0 as i64, src.1 as i64)
-}
-
 pub fn read_file(file_path: &str) -> io::Result<String> {
     let mut file = File::open(file_path)?;
     let mut contents = String::new();
@@ -19,7 +15,7 @@ pub fn get_day_input_content(day: &mut u8, year: u16, test: bool) -> String {
         *day = date.try_into().unwrap();
     }
 
-    let file_name = format!("data/inputs/{year}/day{day:02}{}.txt", if test {".test"} else {""});
+    let file_name = format!("inputs/{year}/day{day:02}{}.txt", if test {".test"} else {""});
     match read_file(&file_name) {
         Ok(contents) => {
             return contents;
