92f6a20794a14bba4c7c893244e98ede73a20cd1
bdef-adv <baptiste.defrance@advens.fr>
2024-12-08 14:30:31 +0100

    day 18 working

--- 2023/18.py
+++ 2023/18.py
@@ -56,55 +56,6 @@ class Point:
             return Point(self.y * other, self.x * other)
         return Point(self.y * other.y, self.x * other.x)
 
-    def in_map(self):
-        """ Is point in map? """
-        global POLYGON
-        return POLYGON.contains(shapely.geometry.Point(self.y, self.x))
-
-    def is_corner_paths(self, paths):
-        """ count number of '#' adjacent to current position """
-        if self not in paths:
-            return False
-
-        right = Point(self.y, self.x + 1) in paths
-        left = Point(self.y, self.x - 1) in paths
-        top = Point(self.y - 1, self.x) in paths
-        down = Point(self.y + 1, self.x) in paths
-
-        return ((right and down) or (right and top) or
-                (down and left) or (down and right) or
-                (top and left) or (top and right) or
-                (left and down) or (left and top))
-
-    def is_corner(self, puzzle):
-        """ count number of '#' adjacent to current position """
-        if puzzle[self.y][self.x] == '.':
-            return 0
-        edges = 0
-        left, right, down, top = False, False, False, False
-        # right
-        if self.x + 1 < len(puzzle[0]):
-            right = puzzle[self.y][self.x + 1] == '#'
-            edges += int(puzzle[self.y][self.x + 1] == '#')
-        # left
-        if self.x - 1 >= 0:
-            left = puzzle[self.y][self.x - 1] == '#'
-            edges += int(puzzle[self.y][self.x - 1] == '#')
-        # top
-        if self.y - 1 >= 0:
-            top = puzzle[self.y - 1][self.x] == '#'
-            edges += int(puzzle[self.y - 1][self.x] == '#')
-        # down
-        if self.y + 1 < len(puzzle):
-            down = puzzle[self.y + 1][self.x] == '#'
-            edges += int(puzzle[self.y + 1][self.x] == '#')
-
-        return ((right and down) or (right and top) or
-                (down and left) or (down and right) or
-                (top and left) or (top and right) or 
-                (left and down) or (left and top) or
-                (self.x - 1 < 0 and edges == 1))
-
 DIRECTION_TO_STR = {
     Point(0, 1): "R",
     Point(1, 0): "D",
@@ -112,56 +63,6 @@ DIRECTION_TO_STR = {
     Point(-1, 0): "U"
 }
 STR_TO_DIRECTION = {r: l for l, r in DIRECTION_TO_STR.items()}
-
-def display_map(puzzle):
-    """ display 2d map """
-    print("Map:")
-    for l in puzzle:
-        print(l)
-
-def get_edges_paths(paths):
-    """ get edges """
-    edges = []
-    for point in paths:
-        if point.is_corner_paths(paths):
-            edges.append((point.y, point.x))
-    return edges
-
-def count_all_positions(paths, maximum, minimum):
-    """ count all without having to draw a map """  
-    min_y, min_x = minimum
-    max_y, max_x = maximum
-    count = 0
-    for y in range(min_y - 1, max_y + 3):
-        for x in range(min_x - 1, max_x + 3):
-            if Point(y, x) not in paths:
-                if Point(y, x).in_map():
-                    count += 1
-            else:
-                count += 1
-    return count
-
-def get_area(points):
-    """ det(A)=ad−bc
-        | a b |
-        | c d |
-        | p1.y p2.y |
-        | p1.x p2.x |
-    """
-    area = 0
-    last_point = Point(points[0])
-    for point in points[1::]:
-        point = Point(point)
-        area += last_point.x * point.y - last_point.y * point.x
-    return area // 2
-
-def PolyArea(x,y):
-    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
-
-def polygon_area(x,y):
-    correction = x[-1] * y[0] - y[-1]* x[0]
-    main_area = np.dot(x[:-1], y[1:]) - np.dot(y[:-1], x[1:])
-    return 0.5*np.abs(main_area + correction)
 
 def solution_part1(filename):
     """ PART 1
@@ -180,50 +81,45 @@ def solution_part1(filename):
             line = _line.rstrip()
             direction, depth, _ = INPUT_RE.findall(line)[0]
             direction = STR_TO_DIRECTION[direction]
-            for _ in range(int(depth)):
-                position += direction #* int(depth)
-                min_y = min(position.y, min_y)
-                min_x = min(position.x, min_x)
-                max_y = max(position.y, max_y)
-                max_x = max(position.x, max_x)
-                if position not in digpos:
-                    digpos.append(position)
+            position += direction * int(depth)
+            min_y = min(position.y, min_y)
+            min_x = min(position.x, min_x)
+            max_y = max(position.y, max_y)
+            max_x = max(position.x, max_x)
+            if position not in digpos:
+                digpos.append(position)
 
-        edges = get_edges_paths(digpos)
+        edges = [(p.y, p.x) for p in digpos] 
         POLYGON = shapely.geometry.Polygon(edges)
-        print(len(digpos), len(POLYGON.exterior.coords), get_area(POLYGON.exterior.coords), POLYGON.area)
-        return count_all_positions(digpos, (max_y, max_x), (min_y, min_x))
+        return int(abs(POLYGON.area) + POLYGON.length/2 + 1)
 
 def solution_part2(filename):
     """ PART 2
     """
     global POLYGON
     with open(filename, "r", encoding="utf-8") as file:
-        s = 800000
-        digpos = [Point(s, s)]
+        digpos = [Point(0,0)]
         max_x = 0
         max_y = 0
         min_x = 0
         min_y = 0
-        position = Point(s, s)
+        position = Point(0, 0)
         for _line in file:
             line = _line.rstrip()
             _, _, color = INPUT_RE.findall(line)[0]
             depth = int(color[:-1:], 16)
             direction = STR_TO_DIRECTION[list(STR_TO_DIRECTION.keys())[int(color[-1])]]
-            for _ in range(int(depth)):
-                position += direction
-                min_y = min(position.y, min_y)
-                min_x = min(position.x, min_x)
-                max_y = max(position.y, max_y)
-                max_x = max(position.x, max_x)
-                if position not in digpos:
-                    digpos.append(position)
+            position += direction * int(depth)
+            min_y = min(position.y, min_y)
+            min_x = min(position.x, min_x)
+            max_y = max(position.y, max_y)
+            max_x = max(position.x, max_x)
+            if position not in digpos:
+                digpos.append(position)
 
-        edges = get_edges_paths(digpos)
+        edges = [(p.y, p.x) for p in digpos] 
         POLYGON = shapely.geometry.Polygon(edges)
-        return count_all_positions(digpos, (max_y, max_x), (min_y, min_x))
-
+        return int(abs(POLYGON.area) + POLYGON.length/2 + 1)
 
 if __name__ == "__main__":
     print("--- Part One ---")
