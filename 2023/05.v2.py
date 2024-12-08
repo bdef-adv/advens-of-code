9d3cb4ba12786a847f438d8046fe4789d6015f54
bdef-adv <baptiste.defrance@advens.fr>
2024-12-08 14:30:30 +0100

    yo la team

--- 2023/05.v2.py
+++ 2023/05.v2.py
@@ -77,40 +77,27 @@ def solution_part2(filename):
                 elif current_map:
                     convert_maps[current_map].append(list(map(int, line.split())))
 
+        from progressbar import progressbar
+
         min_location = None
-        for i in range(0, len(seeds), 2):
-            seed_start, seed_end = seeds[i], seeds[i]+seeds[i+1]
-            data = {"soil": None, "fertilizer": None, "water": None, "light": None, "temperature":None, "humidity": None, "location": None}
-            for name, convert_map in convert_maps.items():
-                l, r = name.split("-to-")
-                for m in convert_map:
-                    dest_range_start, src_range_start, range_len = m
+        for i in range(6, len(seeds), 2):
+            for seed in progressbar(range(seeds[i], seeds[i]+seeds[i+1])):
+                data = {"soil": None, "fertilizer": None, "water": None, "light": None, "temperature":None, "humidity": None, "location": None}
+                for name, convert_map in convert_maps.items():
+                    l, r = name.split("-to-")
+                    for m in convert_map:
+                        dest_range_start, src_range_start, range_len = m
 
-                    s = data[l] if l in data and data[l] else (seed_start, seed_end)
+                        s = data[l] if l in data and data[l] else seed
+                        if src_range_start <= s < src_range_start + range_len:
+                            data[r] = dest_range_start + (s - src_range_start)
+                        elif l in data and not data[r]:
+                            data[r] = s
+                        elif not data[r]:
+                            data[r] = seed
 
-                    print(f"({seed_start},{seed_end}) ({s[0]},{s[1]}) => {l} to {r} {dest_range_start} {src_range_start} {range_len}")
-
-                    ## ARE THEY SEEDS IN THE DEST RANGE?
-                    if src_range_start <= s[0] < src_range_start + range_len:
-                        offset = s[0] - src_range_start
-                        left, right = (dest_range_start + offset, s[1])
-                        data[r] = (left, right)
-                        print("S[0] =>", left, right)
-                    elif  src_range_start <= s[1] < src_range_start + range_len:
-                        offset = s[1] - src_range_start
-                        left, right = (s[0], dest_range_start)
-                        data[r] = (left, right)
-                        print("S[1] =>", offset, left, right)
-                    ## NOT IN RANGE, CONVERT
-                    elif l in data and not data[r]:
-                        data[r] = s
-                    ## KEEP
-                    elif not data[r]:
-                        data[r] = (seed_start, seed_end)
-
-                if data["location"]:
-                    min_location = data["location"][0] if not min_location else min(data["location"][0], min_location)
-            print(f"{seed_start},{seed_end} ({data}) => {min_location}")
+                min_location = data["location"] if not min_location else min(data["location"], min_location)
+            print(min_location)
             
         return min_location
 
@@ -127,4 +114,4 @@ if __name__ == "__main__":
     print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.test.txt"))
 
     print("Result:")
-    #print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.txt"))
+    print(solution_part2(f"input.{FILENAME_TRUNC}{FILENAME_PART2_EXT}.txt"))
