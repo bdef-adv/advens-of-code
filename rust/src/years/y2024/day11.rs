d2122b0251bda3fea084aa316572721aedea5dd1
bdef-adv <baptiste.defrance@advens.fr>
2024-12-11 07:26:38 +0100

    test opti vite zef

--- rust/src/years/y2024/day11.rs
+++ rust/src/years/y2024/day11.rs
@@ -1,17 +1,22 @@
 fn blink(stones: &mut Vec<String>) -> Vec<String> {
     let mut new_stones: Vec<String> = vec![];
-    for stone in stones {
+    let offset: u64 = 0;
+    for (index, stone) in stones.clone().iter().enumerate() {
         if stone == "0" {
-            new_stones.push("1".to_string());
+            stones[index] = "1".to_string();
+            //new_stones.push("1".to_string());
         } else if stone.len() % 2 == 0 {
             let (left, right) = stone.split_at(stone.len() / 2);
             let left_n: u64 = left.parse().unwrap();
             let right_n: u64 = right.parse().unwrap();
-            new_stones.push(left_n.to_string());
-            new_stones.push(right_n.to_string());
+            stones[index] = left_n.to_string();
+            stones.insert(index + 1, right_n.to_string());
+            /*new_stones.push(left_n.to_string());
+            new_stones.push(right_n.to_string());*/
         } else {
             let stone_as_int: u64 = stone.parse().unwrap();
-            new_stones.push((stone_as_int*2024).to_string());
+            stones[index] = (stone_as_int*2024).to_string();
+            //new_stones.push((stone_as_int*2024).to_string());
         }
     }
     return new_stones;
@@ -24,14 +29,15 @@ fn solve_day(file_contents: &str) -> (usize, usize) {
     let mut stones: Vec<String> = file_contents.split_whitespace().map(|s| s.to_string()).collect();
 
     for _ in 0..25 {
-        stones = blink(&mut stones);
+        println!("Blink {}, {:?}", i+1, stones);
+        blink(&mut stones);
     }
 
     let sum_part1: usize = stones.len();
 
     for i in 0..50 {
         println!("Blink {}", i+26);
-        stones = blink(&mut stones);
+        blink(&mut stones);
     }
 
     let sum_part2: usize = stones.len();
