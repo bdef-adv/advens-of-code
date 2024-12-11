b21895889d9a354584d64ff00679a189f44dba2f
bdef-adv <baptiste.defrance@advens.fr>
2024-12-11 07:15:33 +0100

    day 11

--- rust/src/years/y2024/day11.rs
+++ rust/src/years/y2024/day11.rs
@@ -1,11 +1,39 @@
-fn solve_day(_file_contents: &str) -> (u32, u32) {
+fn blink(stones: &mut Vec<String>) -> Vec<String> {
+    let mut new_stones: Vec<String> = vec![];
+    for stone in stones {
+        if stone == "0" {
+            new_stones.push("1".to_string());
+        } else if stone.len() % 2 == 0 {
+            let (left, right) = stone.split_at(stone.len() / 2);
+            let left_n: u64 = left.parse().unwrap();
+            let right_n: u64 = right.parse().unwrap();
+            new_stones.push(left_n.to_string());
+            new_stones.push(right_n.to_string());
+        } else {
+            let stone_as_int: u64 = stone.parse().unwrap();
+            new_stones.push((stone_as_int*2024).to_string());
+        }
+    }
+    return new_stones;
+}
+
+fn solve_day(file_contents: &str) -> (usize, usize) {
     /*
         Part 1
      */
-    let mut sum_part1: u32 = 0;
-    let mut sum_part2: u32 = 0;
-    sum_part1 += 0;
-    sum_part2 += 0;
+    let mut stones: Vec<String> = file_contents.split_whitespace().map(|s| s.to_string()).collect();
+
+    for _ in 0..25 {
+        stones = blink(&mut stones);
+    }
+
+    let sum_part1: usize = stones.len();
+
+    for _ in 0..50 {
+        stones = blink(&mut stones);
+    }
+
+    let sum_part2: usize = stones.len();
     
     (sum_part1, sum_part2)
 }
