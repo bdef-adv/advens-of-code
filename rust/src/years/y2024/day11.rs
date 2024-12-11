577da8941a334ba23ebf3c989ed2bea7feddaf77
bdef-adv <baptiste.defrance@advens.fr>
2024-12-11 10:32:48 +0100

    day 11

--- rust/src/years/y2024/day11.rs
+++ rust/src/years/y2024/day11.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+// Merci chatgpt je suis trop un flemmard
 fn split_number(n: u64) -> (u64, u64) {
     let mut temp = n;
     let mut digits = 0;
@@ -28,61 +31,58 @@ fn has_even_length(n: u64) -> bool {
     digits % 2 == 0
 }
 
-fn blink_int(stones: &mut Vec<u64>) -> Vec<u64> {
-    let mut new_stones: Vec<u64> = vec![];
+fn blink_stone(stone: u64) -> (u64, Option<u64>) {
+    if stone == 0 {
+        return (1, None);
+    } else if has_even_length(stone) {
+        let (left, right) = split_number(stone);
+        return (left, Some(right));
+    } else {
+        return (stone*2024, None);
+    }
+}
+
+// Les nombres se repetent pas mal, plutot que stocker tous les chiffres dans un vecteur 
+// qui s'aggrandit beaucoup trop vite, on utilise un hashmap pour stocker les comptes des nombres qu'on a déjà vu.
+// Abusé la diff parceque l'implementation d'avant arrivait en OOM sur mes 32Go de RAM à l'étape 49
+fn blink_int_3(stones: &mut HashMap<u64, u64>) -> u64 {  
+    let mut new_stones: HashMap<u64, u64> = HashMap::new();
+    let mut total_count: u64 = 0;
+
+    for (&stone, count) in stones.iter_mut() {
+        let (left, r) = blink_stone(stone);
+
+        *new_stones.entry(left).or_insert(0) += *count;
+        total_count += *count;
+
+        if let Some(right) = r {
+            *new_stones.entry(right).or_insert(0) += *count;
+            total_count += *count;
+        }
+    }
+
+    *stones = new_stones;
+
+    return total_count;
+}
+
+
+fn solve_day(file_contents: &str) -> (u64, u64) {
+    let stones: Vec<u64> = file_contents.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let mut stone_map: HashMap<u64, u64> = HashMap::new();
     for &stone in stones.iter() {
-        if stone == 0 {
-            new_stones.push(1);
-        } else if has_even_length(stone) {
-            let (left, right) = split_number(stone);
-            new_stones.push(left);
-            new_stones.push(right);
-        } else {
-            new_stones.push(stone*2024)
-        }
-    }
-    return new_stones;
-}
-
-fn blink_int_2(stones: &mut Vec<u64>) {
-    let stones_len: usize = stones.len();
-    
-    for index in 0..stones_len {
-        let stone = stones[index];
-        if stone == 0 {
-            stones[index] = 1;
-        } else if has_even_length(stone) {
-            let (left, right) = split_number(stone);
-            stones[index] = left;
-            stones.push(right);
-        } else {
-            stones[index] = stone*2024;
-        }
-    }
-}
-
-
-fn solve_day(file_contents: &str) -> (usize, usize) {
-    /*
-        Part 1
-     */
-    let mut stones: Vec<u64> = file_contents.split_whitespace().map(|s| s.parse().unwrap()).collect();
-
-    for i in 0..25 {
-        blink_int_2(&mut stones);
-        println!("blink {i}: {:?}", stones);
+        stone_map.entry(stone).or_insert(1);
     }
 
-    let sum_part1: usize = stones.len();
-
-    println!("PART 1: {sum_part1}");
-
-    for i in 0..50 {
-        println!("Blink {}", i+26);
-        blink_int_2(&mut stones);
+    let mut sum_part1: u64 = 0;
+    for _ in 0..25 {
+        sum_part1 = blink_int_3(&mut stone_map);
     }
 
-    let sum_part2: usize = stones.len();
+    let mut sum_part2: u64 = 0;
+    for _ in 0..50 {
+        sum_part2 = blink_int_3(&mut stone_map);
+    }
     
     (sum_part1, sum_part2)
 }
