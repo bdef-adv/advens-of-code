5b5ea458637827a3c977469aefe29a4b1a9d5c35
bdef-adv <baptiste.defrance@advens.fr>
2024-12-22 21:42:25 +0100

    unsafe baby yeah

--- rust/src/years/y2024/day19.rs
+++ rust/src/years/y2024/day19.rs
@@ -1,11 +1,43 @@
-fn solve_day(_file_contents: &str) -> (u32, u32) {
+use regex::Regex;
+
+fn towel_ok(towel: &str, re: &Regex) -> bool {
+    return re.is_match(towel);
+}
+
+static mut AVAILABLE: Vec<String> = vec![];
+
+fn solve_day(file_contents: &str) -> (u32, u32) {
     /*
         Solve day
      */
+    let mut models_to_try: Vec<&str> = vec![];
+    let mut models_passed = false;
+    for line in file_contents.lines() {
+        if line.is_empty() {
+            models_passed = true;
+            continue;
+        }
+        if !models_passed {
+            unsafe {
+                AVAILABLE = line.split(", ").collect::<Vec<&str>>().iter().map(|s| s.to_string()).collect();
+            }
+        } else {
+            models_to_try.push(line);
+        }
+    }
+
     let mut sum_part1: u32 = 0;
-    let mut sum_part2: u32 = 0;
-    sum_part1 += 0;
-    sum_part2 += 0;
+    let sum_part2: u32 = 0;
+    unsafe {
+        let re_pat = format!("^({})+$", AVAILABLE.join("|"));
+        let re = Regex::new(&re_pat).unwrap();
+
+        for &model in models_to_try.iter() {
+            if towel_ok(model, &re) {
+                sum_part1 += 1;
+            }
+        }
+    }
     
     (sum_part1, sum_part2)
 }
