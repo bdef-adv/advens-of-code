f41d09d60da05c7e6d4240c6133465ba788aa3df
bdef-adv <baptiste.defrance@advens.fr>
2025-12-04 05:56:49 +0100

    day 04 prep

---
+++ rust/src/years/y2025/day04.rs
@@ -0,0 +1,26 @@
+fn part01(file_contents: &str) -> u64 {
+    /*
+        Part 1
+     */
+    let mut sum: u64 = 0;
+
+    return sum;
+}
+
+
+fn part02(file_contents: &str) -> u64 {
+    /*
+        Part 2
+     */
+    let mut sum: u64 = 0;
+
+    return sum;
+}
+
+
+pub fn get_day_results(file_contents: &str) -> (String, String) {
+    /*
+        Return this day's results as a tuple
+     */
+    (format!("{}", part01(&file_contents)), format!("{}", part02(&file_contents)))
+}
