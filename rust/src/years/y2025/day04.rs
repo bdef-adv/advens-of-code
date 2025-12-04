904165efeb9c1854a1e332d6ccff473b896cdf78
bdef-adv <baptiste.defrance@advens.fr>
2025-12-04 06:30:54 +0100

    day 04 part 2

--- rust/src/years/y2025/day04.rs
+++ rust/src/years/y2025/day04.rs
@@ -11,12 +11,10 @@ fn part01(file_contents: &str) -> u64 {
     let mut sum: u64 = 0;
 
     let maze = MazeChar::from(file_contents);
-    maze.print();
-
     for y in 0..maze.size_y {
         for x in 0..maze.size_x {
             let pos = Point32::from(x as i32, y as i32);
-            if maze.count_neighbors(&pos, &'@') < 4 {
+            if maze.get(&pos) == Some('@') && maze.count_neighbors(&pos, &'@') < 4 {
                 sum += 1;
             }
         }
@@ -31,6 +29,32 @@ fn part02(file_contents: &str) -> u64 {
         Part 2
      */
     let mut sum: u64 = 0;
+
+    let mut maze = MazeChar::from(file_contents);
+
+    loop {
+        let mut removed: Vec<Point32> = Vec::with_capacity(maze.size_x * maze.size_y);
+        let mut current_sum: u64 = 0;
+
+        for y in 0..maze.size_y {
+            for x in 0..maze.size_x {
+                let pos = Point32::from(x as i32, y as i32);
+                if maze.get(&pos) == Some('@') && maze.count_neighbors(&pos, &'@') < 4 {
+                    removed.push(pos);
+                    current_sum += 1;
+                }
+            }
+        }
+
+        sum += current_sum;
+        if current_sum == 0 {
+            break;
+        }
+
+        for pos in removed {
+            maze.set(&pos, '.');
+        }
+    }
 
     return sum;
 }
